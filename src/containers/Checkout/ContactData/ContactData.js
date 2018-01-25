218d88f22df6d3b12e9adc2c486e5c222b641ca4
Roberto Melfi <>
2018-01-25 14:55:55 +0100

    222. Handling User Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,6 +81,19 @@ class ContactData extends Component {
             });
     };
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updateFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updateFormElement.value = event.target.event;
+        updatedOrderForm[inputIdentifier] = updateFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    };
+
     render () {
 
         const formElementsArray = [];
@@ -106,7 +119,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value}/>
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
             </div>
         );

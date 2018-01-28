791acb2c334251e4e9473869da4131f6b9d19845
Roberto Melfi <>
2018-01-28 19:24:15 +0100

    257. Finishing the Reducer for Ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,16 +30,16 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount () {
-        // console.log(this.props);
-        axios.get('https://react-my-burger-646ee.firebaseio.com/ingredients.json')
-            .then( response => {
-                this.setState({ingredients: response.data});
-                //this.updateTotalPrice(this.state.ingredients);
-                //this.updatePurchaseState(this.state.ingredients);
-            })
-            .catch( error => {
-                this.setState({error: true});
-            });
+        console.log(this.props);
+        // axios.get('https://react-my-burger-646ee.firebaseio.com/ingredients.json')
+        //     .then( response => {
+        //         this.setState({ingredients: response.data});
+        //         //this.updateTotalPrice(this.state.ingredients);
+        //         //this.updatePurchaseState(this.state.ingredients);
+        //     })
+        //     .catch( error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updateTotalPrice (ingredients) {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,36 @@
 import * as actionfTypes from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case actionfTypes.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionfTypes.REMOVE_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;

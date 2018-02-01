10d18f154d49eaed72a7a389338fc3d14eb3acc5
Roberto Melfi <>
2018-02-01 15:20:14 +0100

    fix redux dev tools in production mode

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+//const composeEnhancers = compose;
 
 const rootReducer = combineReducers({
         burgerBuilder: burgerBuilderReducer,

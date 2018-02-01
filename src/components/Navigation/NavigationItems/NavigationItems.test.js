81895aa2b54ec937f4cffe00fe92f5565e5145b5
Roberto Melfi <>
2018-02-01 12:09:32 +0100

    338. Test (does not work)

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,14 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
-    it('should render two <NavigationItem />  elements if not authenticated', () => {
+
+    it('should render two elements if not authenticated', () => {
         const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three elements if  authenticated', () => {
+        const wrapper = shallow(<NavigationItems isAuthenticated/>);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
